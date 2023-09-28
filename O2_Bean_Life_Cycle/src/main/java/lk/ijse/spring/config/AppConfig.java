f499f8823b4bf375c815824ac988d179ac89cde9
Gathsara2002 <liviru2002mmg@gmail.com>
2023-09-28 19:39:25 +0530

    check configuration class life cycle

--- O2_Bean_Life_Cycle/src/main/java/lk/ijse/spring/config/AppConfig.java
+++ O2_Bean_Life_Cycle/src/main/java/lk/ijse/spring/config/AppConfig.java
@@ -1,5 +1,9 @@
 package lk.ijse.spring.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,8 +13,33 @@ import org.springframework.context.annotation.Configuration;
  **/
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.spring")
-public class AppConfig {
+public class AppConfig implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
     public AppConfig() {
         System.out.println("AppConfig - Instantiate");
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("AppConfig - Bean name awareness");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("AppConfig - Bean factory awareness");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("AppConfig - Application context awareness");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("AppConfig - Initializing");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("AppConfig - Death");
     }
 }
