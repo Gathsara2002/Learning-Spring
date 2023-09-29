6eef151009a2847cfe5ea55ef6a3067aedea301f
Gathsara2002 <liviru2002mmg@gmail.com>
2023-09-29 10:22:08 +0530

    Added new module

---
+++ O3_Bean_Scopes/src/main/java/lk/ijse/spring/AppInitializer.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring;
+
+/**
+ * @author : Gathsara
+ * created : 9/29/2023 -- 10:19 AM
+ **/
+public class AppInitializer {
+    public static void main(String[] args) {
+        System.out.println("Testing");
+    }
+}
