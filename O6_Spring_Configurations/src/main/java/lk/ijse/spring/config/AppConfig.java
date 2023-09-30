b5ad143c32bb7ceeeaff316691d7199ee97ee29b
Gathsara2002 <liviru2002mmg@gmail.com>
2023-09-30 20:20:44 +0530

    check @ImportResource annotation usage

--- O6_Spring_Configurations/src/main/java/lk/ijse/spring/config/AppConfig.java
+++ O6_Spring_Configurations/src/main/java/lk/ijse/spring/config/AppConfig.java
@@ -3,6 +3,7 @@ package lk.ijse.spring.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author : Gathsara
@@ -12,6 +13,8 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.spring.pojo")
 @Import({ConfigOne.class, ConfigTwo.class})
+//@ImportResource("classpath: abc.xml")
+//@ImportResource("filepath:abc.xml")
 public class AppConfig {
     public AppConfig() {
         System.out.println("AppConfig Instantiated");
