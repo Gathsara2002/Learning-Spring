358fa255552c90f2b0fb98041556283602218505
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-01 20:22:11 +0530

    check @Value annotation usage

--- O7_Runtime_Value_Injection/src/main/java/lk/ijse/spring/pojo/Customer.java
+++ O7_Runtime_Value_Injection/src/main/java/lk/ijse/spring/pojo/Customer.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Customer {
-    public Customer() {
-        System.out.println("Customer Instantiated");
+    public Customer(@Value("IJSE") String name) {
+        System.out.println("Customer Instantiated " + name);
     }
 }
