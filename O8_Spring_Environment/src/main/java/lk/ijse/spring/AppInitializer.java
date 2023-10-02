8ad19d6370ead3d5b366a2e9a98d051cb8b96b89
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-02 20:01:37 +0530

    gest OS and Java environment variables

--- O8_Spring_Environment/src/main/java/lk/ijse/spring/AppInitializer.java
+++ O8_Spring_Environment/src/main/java/lk/ijse/spring/AppInitializer.java
@@ -3,6 +3,10 @@ package lk.ijse.spring;
 import lk.ijse.spring.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
 /**
  * @author : Gathsara
  * created : 10/2/2023 -- 7:30 PM
@@ -13,5 +17,22 @@ public class AppInitializer {
         ctx.register(AppConfig.class);
         ctx.refresh();
         ctx.registerShutdownHook();
+
+        //OS environment variables
+        Map<String, String> getenv = System.getenv();
+        for (String key : getenv.keySet()) {
+            String value = getenv.get(key);
+            System.out.println(key + " : " + value);
+        }
+
+        System.out.println("------------------------------------------");
+
+        //Java environment variables
+        Properties properties = System.getProperties();
+        for (Object key : properties.keySet()) {
+            Object value = properties.get(key);
+            System.out.println(key + " : " + value);
+        }
+
     }
 }
