5b99fe78f274eeab591381ce8dfac2ee269f324b
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-03 15:14:18 +0530

    content updated

--- O9_Spring_Web_MVC/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ O9_Spring_Web_MVC/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = "lk.ijse.spring.pojo")
+@ComponentScan(basePackages = {"lk.ijse.spring.pojo", "lk.ijse.spring.controller"})
 public class WebAppConfig {
     public WebAppConfig() {
         System.out.println("WebAppConfig Instantiated");
---
+++ O9_Spring_Web_MVC/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -0,0 +1,20 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Gathsara
+ * created : 10/3/2023 -- 3:07 PM
+ **/
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @GetMapping
+    public void callMe(){
+        System.out.println("Call me method invoked");
+    }
+}
