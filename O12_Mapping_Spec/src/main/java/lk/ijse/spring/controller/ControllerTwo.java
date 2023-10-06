e5d8072e73b3f5564ad0e2e893046c2086abb31b
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-06 13:04:02 +0530

    Added ControllerTwo

---
+++ O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/ControllerTwo.java
@@ -0,0 +1,14 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Gathsara
+ * created : 10/6/2023 -- 1:03 PM
+ **/
+
+@RestController
+@RequestMapping("/two")
+public class ControllerTwo {
+}
