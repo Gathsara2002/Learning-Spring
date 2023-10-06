770ca2b7c8c56749ca10f8e11755c615fc3f1bc2
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-06 16:52:37 +0530

    Added ControllerThree

---
+++ O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/ControllerThree.java
@@ -0,0 +1,15 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Gathsara
+ * created : 10/6/2023 -- 4:49 PM
+ **/
+
+@RestController
+@RequestMapping("/three")
+public class ControllerThree {
+
+}
