5743553a123cbc9404d099ada53beeea5a038dd0
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-06 12:43:27 +0530

    Added new web module

---
+++ O12_Mapping_Spec/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : Gathsara
+ * created : 10/6/2023 -- 12:40 PM
+ **/
+
+@Configuration
+public class WebRootConfig {
+}
---
+++ O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/ControllerOne.java
@@ -0,0 +1,14 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Gathsara
+ * created : 10/6/2023 -- 12:41 PM
+ **/
+
+@RestController
+@RequestMapping("/one")
+public class ControllerOne {
+}
