4d08e5669e85e9f34a319469c9d999c376532f1d
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-06 17:09:51 +0530

    check path variables

--- O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/ControllerThree.java
+++ O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/ControllerThree.java
@@ -1,5 +1,7 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/three")
 public class ControllerThree {
 
+    @GetMapping(path = "/id/{C001}/{Gathsara}")
+    public String testOne(@PathVariable("C001") String id, @PathVariable("Gathsara") String name) {
+        System.out.println(id);
+        System.out.println(name);
+        return "GetMapping 1 invoked";
+    }
 }
