4f3bcb749d684266ac2398f3f23589b304416412
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-07 12:14:42 +0530

    Added new controller class

---
+++ O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/ControllerFive.java
@@ -0,0 +1,14 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Gathsara
+ * created : 10/7/2023 -- 12:13 PM
+ **/
+
+@RestController
+@RequestMapping("/five")
+public class ControllerFive {
+}
