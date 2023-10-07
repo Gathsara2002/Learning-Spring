2149d68e9550c2e1807767a8b82345318a41f8d1
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-07 13:05:24 +0530

    Request headers

--- O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/ControllerSix.java
+++ O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/ControllerSix.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/six")
 public class ControllerSix {
+
+    @GetMapping(consumes = "application/json")
+    public String testOne() {
+        return "GetMapping 1 invoked";
+    }
+
+    @GetMapping
+    public String testTwo() {
+        return "GetMapping 2 invoked";
+    }
 }
