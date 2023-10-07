6a8488b71cd5eb31baf2fcf51e30c447b364b1ab
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-07 12:41:32 +0530

    content updated with @RequestParam

--- O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/ControllerFive.java
+++ O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/ControllerFive.java
@@ -2,6 +2,7 @@ package lk.ijse.spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,8 +20,8 @@ public class ControllerFive {
     }
 
     @GetMapping(params = {"address", "salary"})
-    public String testTwo() {
-        return "GetMapping 2 invoked";
+    public String testTwo(@RequestParam String address, @RequestParam String salary) {
+        return "GetMapping 2 invoked" + address + "  " + salary;
     }
 
 }
