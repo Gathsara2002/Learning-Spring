483cb4a0198c08f663b9d9284d5a9550ad6eab67
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-07 12:39:43 +0530

    Query string parameters

--- O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/ControllerFive.java
+++ O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/ControllerFive.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/five")
 public class ControllerFive {
+
+    @GetMapping(params = {"id", "name"})
+    public String testOne() {
+        return "GetMapping 1 invoked";
+    }
+
+    @GetMapping(params = {"address", "salary"})
+    public String testTwo() {
+        return "GetMapping 2 invoked";
+    }
+
 }
