e4f1b67573ed019cf4e66ed92f741b2c05c105e2
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-12 21:20:19 +0530

    Added new controller

---
+++ O12_Mapping_Spec/src/main/java/lk/ijse/spring/controller/ControllerEight.java
@@ -0,0 +1,14 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Gathsara
+ * created : 10/12/2023 -- 9:19 PM
+ **/
+
+@RestController
+@RequestMapping("/eight")
+public class ControllerEight {
+}
