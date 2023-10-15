2c5573fa7a3c78d1e1aa477df47acea462749365
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-15 10:56:20 +0530

    Added new module

---
+++ O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -0,0 +1,16 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author : Gathsara
+ * created : 10/6/2023 -- 12:39 PM
+ **/
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"lk.ijse.spring.controller", "lk.ijse.spring.advisor"})
+public class WebAppConfig {
+}
---
+++ O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -0,0 +1,45 @@
+package lk.ijse.spring.controller;
+
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+/**
+ * @author : Gathsara
+ * created : 10/13/2023 -- 5:43 PM
+ **/
+
+@RestController
+@RequestMapping("/customer")
+@CrossOrigin
+public class CustomerController {
+
+    @GetMapping
+    public ResponseUtil getAllCustomer() {
+        ArrayList<CustomerDTO> list = new ArrayList<>();
+        list.add(new CustomerDTO("C001", "Gathsara", "Ambalangoda", 1000000000));
+        list.add(new CustomerDTO("C002", "Chavindu", "Ambalangoda", 50000));
+        list.add(new CustomerDTO("C003", "Hirusha", "Galle", 200000));
+        return new ResponseUtil("OK", "Successfully Loaded", list);
+    }
+
+    @PostMapping
+    public ResponseUtil saveCustomer(@ModelAttribute CustomerDTO dto) {
+        if (dto.getId().equals("C001")) {
+            throw new RuntimeException("Customer Already exists");
+        }
+        return new ResponseUtil("OK", "Successfully Added", dto);
+    }
+
+    @DeleteMapping(params = "id")
+    public ResponseUtil deleteCustomer(@RequestParam String id) {
+        return new ResponseUtil("OK", "Successfully Deleted", id);
+    }
+
+    @PutMapping
+    public ResponseUtil updateCustomer(@RequestBody CustomerDTO dto) {
+        return new ResponseUtil("OK", "Successfully Updated", dto);
+    }
+}
