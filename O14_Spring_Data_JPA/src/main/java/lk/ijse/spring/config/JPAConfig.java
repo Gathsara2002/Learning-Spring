034ba4db56dfa25fdba2e3fc09b572bb4b015ee4
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-15 12:59:15 +0530

    Content updated

--- O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 /**
@@ -49,5 +52,10 @@ public class JPAConfig {
         adapter.setGenerateDdl(true); // Generate Data Definition Language (DDL) queries
         adapter.setShowSql(true);
         return adapter;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory factory) {
+        return new JpaTransactionManager(factory);
     }
 }
